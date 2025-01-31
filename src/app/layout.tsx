868280365980ed1a593e9eb56c4112e34dfdfa8f
Alesha Tikhonov <zukko961@gmail.com>
2025-01-31 00:43:09 +0000

    change google script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,20 +20,18 @@ export default function RootLayout({children}: Readonly<{children: React.ReactNo
   return (
     <html lang="en">
       <Head>
-        {/* Google Tag Manager */}
-        <script data-partytown-config dangerouslySetInnerHTML={{
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-B6Z9MJ4EBF"></script>
+        <script dangerouslySetInnerHTML={{
           __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-T49SQQ9L');
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-B6Z9MJ4EBF');
           `,
         }} />
       </Head>
       <body className={`${dmSans.className} antialiased`}>
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T49SQQ9L" height="0" width="0" className="hidden invisible"></iframe></noscript>
-          {children}
+        {children}
         <Footer/>
       </body>
     </html>
