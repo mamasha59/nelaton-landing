f2dc08489afd5abba7653848904d1d64ac1f1a19
Alesha Tikhonov <zukko961@gmail.com>
2025-05-20 11:38:53 +0100

    change google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
-/* eslint-disable @next/next/next-script-for-ga */
 import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer/Footer";
-import Head from "next/head";
+import { GoogleAnalytics } from '@next/third-parties/google';
 import Script from "next/script";
 import {generateSchema} from '@/utils/generateSchema';
 
@@ -22,18 +21,6 @@ export default async function RootLayout({children}: Readonly<{children: React.R
 
   return (
     <html lang="en">
-      {/* Google Tag Manager */}
-      <Script id="gtm-script" strategy="afterInteractive">
-        {`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-T49SQQ9L');
-        `}
-      </Script>
-       {/* Google Analytics */}
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-B6Z9MJ4EBF" strategy="afterInteractive"/>
       <Script id="cookieyes" strategy="afterInteractive" src="https://cdn-cookieyes.com/client_data/72ee03861749bd377718059e/script.js"/>
       <Script
         id="faq-schema"
@@ -42,21 +29,12 @@ export default async function RootLayout({children}: Readonly<{children: React.R
           __html: JSON.stringify(schema),
         }}
       />
-      <Head>
-        <script id="google-analytics" dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-B6Z9MJ4EBF');
-          `,
-        }} />
-      </Head>
       <body className={`${dmSans.className} antialiased`}>
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T49SQQ9L" height="0" width="0" className="hidden invisible"></iframe></noscript>
         {children}
         <Footer/>
       </body>
+      <GoogleAnalytics gaId="G-B6Z9MJ4EBF"/>
     </html>
   );
 }
