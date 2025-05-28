7c687761f4bb8bb5f76ced0806ad2f7bbef7c322
Alesha Tikhonov <zukko961@gmail.com>
2025-05-28 19:44:24 +0100

    add google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,19 @@ export default async function RootLayout({children}: Readonly<{children: React.R
 
   return (
     <html lang="en">
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-T49SQQ9L');
+          `,
+        }}
+      />
       <Script id="cookieyes" strategy="afterInteractive" src="https://cdn-cookieyes.com/client_data/72ee03861749bd377718059e/script.js"/>
       <Script
         id="faq-schema"
@@ -30,7 +43,6 @@ export default async function RootLayout({children}: Readonly<{children: React.R
         }}
       />
       <body className={`${dmSans.className} antialiased`}>
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T49SQQ9L" height="0" width="0" className="hidden invisible"></iframe></noscript>
         {children}
         <Footer/>
       </body>
